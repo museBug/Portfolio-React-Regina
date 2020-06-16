e1b5db7828a6bc857694656921d8757e53dc8a4f
Regina Musovirova <musovirova@yahoo.com>
2020-06-16 14:39:51 -0400

    Updates

---
+++ precache-manifest.f8f05df96b4d499b7c59d6245032b724.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f198d120cf4319abf2c83595879f7ec2",
+    "url": "/Portfolio-React-Regina/index.html"
+  },
+  {
+    "revision": "e0b386c6e5074268d89d",
+    "url": "/Portfolio-React-Regina/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "532254bca464fb7ac05a",
+    "url": "/Portfolio-React-Regina/static/css/main.ed58dfb4.chunk.css"
+  },
+  {
+    "revision": "e0b386c6e5074268d89d",
+    "url": "/Portfolio-React-Regina/static/js/2.c73e4709.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Portfolio-React-Regina/static/js/2.c73e4709.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "532254bca464fb7ac05a",
+    "url": "/Portfolio-React-Regina/static/js/main.f10d5daa.chunk.js"
+  },
+  {
+    "revision": "b883bdf8e9e687cd3bba",
+    "url": "/Portfolio-React-Regina/static/js/runtime-main.79e10743.js"
+  },
+  {
+    "revision": "d63be0f6da1124895e9b5662a944a8b6",
+    "url": "/Portfolio-React-Regina/static/media/Resume_Regina.d63be0f6.pdf"
+  },
+  {
+    "revision": "6f2acab7c8f77bdf41088e8223a28c81",
+    "url": "/Portfolio-React-Regina/static/media/regina.6f2acab7.png"
+  }
+]);
