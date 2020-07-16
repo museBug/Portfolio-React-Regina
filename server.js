25a4c4fedfc044fd1df8e2d2b7461af34e22d4a4
Regina Musovirova <musovirova@yahoo.com>
2020-07-16 12:25:55 -0400

    fixed bug

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname,"build","index"));
 });
 
